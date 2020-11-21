cdc499521a158753a5945e0ade07f16c881fc2d6
sweta sengupta <ssengupta801@gmail.com>
2020-11-20 22:20:47 -0600

    Finished the section

--- adv.py
+++ adv.py
@@ -1,9 +1,17 @@
+
 from room import Room
 from player import Player
 from world import World
 
 import random
 from ast import literal_eval
+
+# Util classes from utils file
+from utils import Stack, Queue
+
+# Helper functions from helpers file
+from helpers import inverse_point, uncharted_path
+
 
 # Load world
 world = World()
@@ -28,6 +36,51 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+# Once this is full, the loop will stop and we will have completed the spec
+visited = {}
+
+# This is the Main loop running while visited is less than # of rooms
+while len(visited) < len(room_graph):
+    # Get current room and it's exits
+    room_id = player.current_room.id
+    room_exits = player.current_room.get_exits()
+
+    # If current room is not in visited, add it
+    if room_id not in visited:
+        visited[room_id] = { points: '?' for points in room_exits }
+    
+    # Get current room's exits
+    uncharted = [point for point in visited[room_id] if visited[room_id][point] == '?']
+    
+    # If uncharted is not an empty list, explore an unknown room
+    if len(uncharted) > 0:
+        # make a random index to go to the next unexplored room
+        next_room_index = random.randint(0, len(uncharted)-1)
+        next_point = uncharted[next_room_index]
+
+        # move to the uncharted room
+        player.travel(next_point)
+        
+        # Add this travel destination to your main path
+        traversal_path.append(next_point)
+        
+        # Now we know what the next room is, set it for the previous room's direction index
+        next_room_id = player.current_room.id
+        visited[room_id][next_point] = next_room_id
+        
+        # If next room is not in visited, add it
+        if next_room_id not in visited:
+            next_room_exits = player.current_room.get_exits()
+            visited[next_room_id] = { points: '?' for points in next_room_exits }
+        
+        # Now we know the room where we came from is the opposite direction of the current room
+        rev_point = inverse_point(next_point)
+        visited[next_room_id][rev_point] = room_id
+
+    # else find your way back to the nearest unexplored room using BFS
+    else:
+        traversal_path += uncharted_path(visited, room_id, player)
+
 
 
 
@@ -51,12 +104,17 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
+
+
+
+
+
